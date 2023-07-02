2b9aaa11025335b36d7aee2db615ad4b0c516871
YashNK <yashkamnani0@gmail.com>
2023-07-02 20:26:23 +0530

    lyrics

--- src/components/Lyrics.jsx
+++ src/components/Lyrics.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 function Lyrics(props) {
   return (
-    <div className='w-full h-[130%] absolute lg:mt-[50%]
-    md:mt-[100%] mt-[209%] bg-black text-white'>
-        <div className='w-full h-screen mt-10 flex items-center flex-col'>
-            <h1>Lyrics</h1>
-            <h1>{props.song.title}</h1>
-            <h2 className='mr-12 ml-12'>{props.song.Lyrics}</h2>
+    <div className='w-full absolute lg:mt-[50%]
+    md:mt-[100%] mt-[209.55%] bg-black text-white'>
+        <div className='w-full bg-black mt-[10%] flex items-center flex-col'>
+            <h1 className='text-[209%]'>Lyrics</h1>
+            <h1 className='mt-3 text-[140%]'>{props.song.title}</h1>
+            <h2 className='mt-4 lyrics-container mr-12 ml-12 text-gray-600'>{props.song.Lyrics}</h2>
+            
         </div>
     </div>
   )
